ad29e4e7fdc349905e92b7f001c0f2624482a289
ruchirbhai <ruchirbhai@gmail.com>
2020-03-23 02:48:42 -0700

    Update demo.py
    Add refactoring code to make the cursor.execute command readable

--- demo.py
+++ demo.py
@@ -22,15 +22,18 @@ cursor.execute("""
 # cursor.execute('INSERT INTO todos (id, description) VALUES (1, true);')
 
 # Exercise 2 now use %s and %(named_var)s to fill the values dynamically
-todo_id = '1'
-todo_status = 'true'
+
+data = {
+    'id': 2,
+    'description': False
+}
+
+SQL = 'INSERT INTO todos (id, description) VALUES (%(id)s, %(description)s);'
+
 cursor.execute('INSERT INTO todos (id, description) VALUES '
                '(%s, %s);',(1, True))
 
-cursor.execute('INSERT INTO todos (id, description) VALUES '
-               '(%(todo_id)s, %(todo_status)s);', {
-                'todo_id': 2,
-                'todo_status': False})
+cursor.execute(SQL, data)
 
 # commit, so it does the executions on the db and persists in the db
 connection.commit()
