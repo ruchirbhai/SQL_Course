120eae3e9c636d5104b08a4dbac1d733d59c2b0c
ruchirbhai <ruchirbhai@gmail.com>
2020-03-23 17:03:49 -0700

    create repr function to make query output reable
    helps with debugging

--- sql_alchemy_demo.py
+++ sql_alchemy_demo.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # Set  configuration variable for flask app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:database@localhost:5433/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # create new instance of the class
 db = SQLAlchemy(app)
@@ -15,10 +16,16 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
 
+    # Create a wrapper functino to display the table in a user friendly way
+    def __repr__(self):
+        return f'<Person ID: {self.id}, name: {self.name}>'
+
 # db.create detects models and creates table for us
 db.create_all()
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    # Querying the name which was added a person name Ruchir manually to the table
+    person = Person.query.first()
+    return 'Hello ' + person.name + '!'
